b2550fbd57e5d9a5892584cf32d101291293d2a9
jiayayu <fsjyy@qq.com>
2017-08-16 15:43:19 +0800

    项目结构搭建完成

---
+++ src/components/header/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是　ｈｅａｄｅｒ
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+    data () {
+      return {
+
+      };
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
