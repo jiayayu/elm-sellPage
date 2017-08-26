83997feb70722a02d51579c5f655b82553758bd3
jiayayu <fsjyy@qq.com>
2017-08-26 18:49:35 +0800

    food页面（组件）全部完成

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,13 +33,35 @@
                         <h1 class="title">商品信息</h1>
                         <p class="text">{{food.info}}</p>
                     </div>
-                    <ratingselect
-                        @select="selectRating"
-                        @toggle="toggleContent"
-                        :onlyContent="onlyContent"
-                        :desc="desc"
-                        :selectType="selectType"
-                        :ratings="food.ratings"></ratingselect>
+                    <split></split>
+                    <div class="rating">
+                        <h1 class="title">商品评价</h1>
+                        <ratingselect
+                            @select="selectRating"
+                            @toggle="toggleContent"
+                            :onlyContent="onlyContent"
+                            :desc="desc"
+                            :selectType="selectType"
+                            :ratings="food.ratings"></ratingselect>
+                        <div class="rating-wrapper">
+                            <ul v-show="food.ratings && food.ratings.length">
+                                <li v-for="rating in food.ratings"
+                                    v-show="needShow(rating.rateType,rating.text)"
+                                    class="rating-item border-1px">
+                                    <div class="user">
+                                        <span class="name">{{rating.username}}</span>
+                                        <img class="avatar" width="12" height="12" :src="rating.avatar">
+                                    </div>
+                                    <div class="time">{{rating.rateTime}}</div>
+                                    <p class="text">
+                                        <span
+                                            :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                                    </p>
+                                </li>
+                            </ul>
+                            <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -83,7 +105,7 @@
                 this.showFlag = true;
                 this.selectType = ALL;
                 this.onlyContent = false;
-                    this.$nextTick(() => {
+                this.$nextTick(() => {
                     if (!this.scroll) {
                         this.scroll = new BScroll(this.$refs.food, {
                             click: true
@@ -95,6 +117,16 @@
             },
             hide() {
                 this.showFlag = false;
+            },
+            needShow(rateType, text) {
+                if (this.onlyContent && !text) {
+                    return false;
+                }
+                if (this.selectType === ALL) {
+                    return true;
+                } else {
+                    return rateType === this.selectType;
+                }
             },
             addFood(target) {
                 this.$emit('add', target);
@@ -123,6 +155,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+    @import "../../common/stylus/mixin.styl"
     .food
         position: fixed
         left: 0
@@ -221,4 +254,53 @@
                 padding: 0 8px
                 font-size: 12px
                 color: rgb(77, 85, 93)
+        .rating
+            padding: 18px
+            .title
+                line-height: 14px
+                margin-bottom: 6px
+                font-size: 14px
+                color: rgb(7, 17, 27)
+            .rating-wrapper
+                padding: 0 18px
+                .rating-item
+                    position: relative
+                    padding: 16px 0
+                    border-1px(rgba(7, 17, 27, 0.1))
+                    .user
+                        position: absolute
+                        right: 0
+                        top: 16px
+                        line-height: 12px
+                        font-size: 0
+                        .name
+                            display: inline-block
+                            margin-right: 6px
+                            vertical-align: top
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
+                        .avatar
+                            border-radius: 50%
+                    .time
+                        margin-bottom: 6px
+                        line-height: 12px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
+                    .text
+                        line-height: 16px
+                        font-size: 12px
+                        color: rgb(7, 17, 27)
+                        .icon-thumb_up, .icon-thumb_down
+                            margin-right: 4px
+                            line-height: 16px
+                            font-size: 12px
+                        .icon-thumb_up
+                            color: rgb(0, 160, 220)
+                        .icon-thumb_down
+                            color: rgb(147, 153, 159)
+
+                .no-rating
+                    padding: 16px 0
+                    font-size: 12px
+                    color: rgb(147, 153, 159)
 </style>
